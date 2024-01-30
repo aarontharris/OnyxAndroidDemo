70f1c941958897c4149bd643c16d6b274c68f9de
aarontharris <aarontharris@gmail.com>
2024-01-30 15:48:48 -0800

    more cleanup

--- app/src/main/java/com/android/onyx/demo/SampleApplication.java
+++ app/src/main/java/com/android/onyx/demo/SampleApplication.java
@@ -1,9 +1,8 @@
 package com.android.onyx.demo;
 
+import android.app.Application;
 import android.content.Context;
 import android.os.Build;
-import android.support.multidex.MultiDex;
-import android.support.multidex.MultiDexApplication;
 
 import com.onyx.android.sdk.rx.RxManager;
 
@@ -13,28 +12,17 @@ import org.lsposed.hiddenapibypass.HiddenApiBypass;
  * Created by suicheng on 2017/3/23.
  */
 
-public class SampleApplication extends MultiDexApplication {
-    private static SampleApplication sInstance;
-
+public class SampleApplication extends Application {
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
-        MultiDex.install(SampleApplication.this);
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        initConfig();
         RxManager.Builder.initAppContext(this);
         checkHiddenApiBypass();
-    }
-
-    private void initConfig() {
-        try {
-            sInstance = this;
-        } catch (Exception e) {
-        }
     }
 
     private void checkHiddenApiBypass() {
